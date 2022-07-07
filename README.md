c8c1974c5755cf134ca950ba9f278e000f399877
felipe-fdmiranda <felipe.fdmiranda@gmail.com>
2022-07-07 02:40:03 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# challenge-nginx-with-node
+Criação de um proxy reverso usando um container Nginx para redirecionar para um container Node
---
+++ node/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(config)
+const createDB = `CREATE TABLE IF NOT EXISTS nodedb.people (id INT auto_increment NOT NULL,` +
+`name varchar(255) NULL,CONSTRAINT people_PK PRIMARY KEY (id));`;
+connection.query(createDB)
+const insertRow = `INSERT INTO nodedb.people(name) values('Felipe');`;
+connection.query(insertRow)
+connection.end()
+
+app.get('/', (req,res) => {
+    const connection = mysql.createConnection(config)
+    connection.query('SELECT * FROM people', function(err, result){
+        console.log(err, result)
+        let resultado = '';
+        result.forEach(people => {
+            resultado += '- ' + people.name + '<br>'
+        });
+        res.send('<h1>Full Cycle - v3</h1>' + '<br>' + resultado)
+    }).on('error', function(err) {
+        console.log("[mysql error]",err);
+    });
+    connection.end();
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
